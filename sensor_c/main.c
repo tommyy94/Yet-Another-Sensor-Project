849e5475f493c23930401ca1af9165ab3ac1a176
tommyy94 <tommy574@protonmail.ch>
2018-08-04 12:49:36 +0300

    Added software reset to sensor_c

--- sensor_c/main.c
+++ sensor_c/main.c
@@ -8,13 +8,30 @@
 
 
 #define IO_SIGNATURE 0xD8
+#define HEARTBEAT
+
+#define SYS_RESET_PIN PINB1
+#define ADC_CHANNEL PINA0
 
 
-inline void clk_init(void);
-inline void WDT_init(void);
+/**
+ * @brief Reset the software by using Watchdog Timer.
+ */
+#define software_reset() /* resets the system */    \
+    do {                                            \
+        wdt_enable(WDTO_15MS);                      \
+        while(1) {                                  \
+            ; /* wait until timer runs out */       \
+        }                                           \
+    } while(0)
+
+
+void WDT_init(void) __attribute__((naked))__attribute__((section(".init3")));
+inline void CLK_init(void);
 inline void UART_init(void);
 inline void ADC_init(void);
 inline void PRR_init(void);
+inline void GPIO_init(void);
 void UART_transmit(unsigned char data);
 void power_down(void);
 void ADC_start_conversion(void);
@@ -27,11 +44,12 @@ void ADC_start_conversion(void);
  */
 int main(void)
 {
-    clk_init();
+    GPIO_init();
+    CLK_init();
     PRR_init();
-    WDT_init();
     UART_init();
     ADC_init();
+    sei();
 
     while(1) {
         ADC_start_conversion();
@@ -49,7 +67,7 @@ int main(void)
  * @param void
  * @return void
  */
-inline void clk_init(void)
+inline void CLK_init(void)
 {
     CCP = IO_SIGNATURE;
     CLKPSR = (1 << CLKPS1) | (1 << CLKPS0);
@@ -62,14 +80,14 @@ inline void clk_init(void)
  * @param void
  * @return void
  */
-inline void WDT_init(void)
+void WDT_init(void)
 {
     WDTCSR = (1 << WDP2) | (1 << WDP1);
 }
 
 
 /**
- * @brief Initializes UART transmission with defined baud rate.
+ * @brief Initializes UART transmission with 8N1 and defined baud rate.
  * @param void
  * @return void
  */
@@ -85,15 +103,15 @@ inline void UART_init(void)
 /**
  * @brief Initializes ADC on channel 0.
  * @details Set ADC prescaler to 8 resulting in 125 kHz resolution. Also enables
- * interrupt on completed conversion.
+ * interrupt on completed conversion. Disables digital input buffer on ADC pin.
  * @param void
  * @return void
  */
 inline void ADC_init(void)
 {
-    ADMUX = (1 << REFS0) | PINA0;
+    ADMUX = (1 << REFS0) | ADC_CHANNEL;
     ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS1) | (1 << ADPS0);
-    DIDR0 = PINA0;
+    DIDR0 = ADC_CHANNEL;
 }
 
 
@@ -109,22 +127,37 @@ inline void PRR_init(void)
 
 
 /**
+ * @brief Initializes IO.
+ * @details Initializes external interrupt too.
+ * @param void
+ * @return void
+ */
+inline void GPIO_init(void)
+{
+    PUEB |= (1 << SYS_RESET_PIN);
+    PORTB |= (1 << SYS_RESET_PIN);
+
+    EICRA = (1 << ISC01);
+    EIMSK = (1 << INT0);
+
+    DDRA |= (1 << DDRA5);
+    PORTA &= ~(1 << PINA5);
+}
+
+
+/**
  * @brief Transmits 8-bit character.
- * @details Waits for empty transmit buffer. Interrupts are disabled during
- * transmission.
+ * @details Waits for empty transmit buffer.
  * @param data : Byte to transmit
  * @return void
  */
 void UART_transmit(unsigned char data)
 {
-    cli();
-
     while(!(UCSRA & (1 << UDRE))) {
         ; /* wait for empty transmit buffer */
     }
 
     UDR = data; /* put data into buffer and send it */
-    sei();
 }
 
 
@@ -150,7 +183,7 @@ void power_down(void)
 
 /**
  * @brief Starts Analog-to-digital conversion by turning off the CPU.
- * @details ADC is power on only during conversion to save power.
+ * @details ADC is powered on only during conversion to save power.
  * @param void
  * @return void
  */
@@ -168,21 +201,32 @@ void ADC_start_conversion(void)
 
 /**
  * @brief Watchdog vector
- * @param void
- * @return void
+ * @details Used to wake up from sleep mode. Pin toggling indicates heartbeat.
  */
 ISR(WDT_vect)
+{
+#ifdef HEARTBEAT
+    PORTA ^= (1 << PORTA5);
+#endif
+}
+
+
+/**
+ * @brief ADC vector
+ */
+ISR(ADC_vect)
 {
     ;
 }
 
 
 /**
- * @brief ADC vector
- * @param void
- * @return void
+ * @brief INT0 vector
+ * @details Resets the system.
  */
-ISR(ADC_vect)
+ISR(INT0_vect)
 {
-    ;
+    if(!(PINB & (1 << SYS_RESET_PIN))) {
+        software_reset();
+    }
 }
